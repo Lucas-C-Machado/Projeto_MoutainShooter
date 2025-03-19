f6c24c31a7293ee3e14cf339cae80397c072dec1
Lucas C. Machado <65616970+Lucas-C-Machado@users.noreply.github.com>
2025-03-19 05:52:15 -0300

    Creating main loop (Criando um loop de menu) Connecting Menu and Game classes (Conectando as classes de Menu e Jogo)

--- code/game.py
+++ code/game.py
@@ -1,24 +1,23 @@
-import pygame
+import pygame #importando a biblioteca pygame
 
-#!/usr/bin/python
+from code.menu import Menu #importando o menu
+
+# !/usr/bin/python
 # -*- coding: utf-8 -*-
 
 class Game:
     def __init__(self):
-        self.window = None
-
-    def run(self, ):
-        print('Setup Start')  # Informa ao usuário que um processo de CONFIGURAÇÃO está INICIANDO
         pygame.init()  # INICIALIZAÇÃO do programa
-        window = pygame.display.set_mode(size=(600, 480))  # Seria a nossa janela de jogo em si
-        # CTRL + ALT + L = Corrige a formatação de TODO o código
-        print('Setup End')  # Informa ao usuário que um processo de CONFIGURAÇÃO está TERMINANDO
+        self.window = pygame.display.set_mode(size=(600, 480))  # Seria a nossa janela de jogo em si
 
-        print('Loop Start')  # Informa ao usuário que um processo de LOOP está INICIANDO
+    def run(self):
         while True:  # A janela fica aberta infinitamente
+            menu = Menu(self.window) #chamando a janela do menu
+            menu.run() #rodando o menu
+            pass
+
             # Check for all events (Cheque por TODOS os eventos)
-            for event in pygame.event.get():  # TODOS os EVENTOS VIRÃO
-                if event.type == pygame.QUIT: #Quando o evento for do tipo quit
-                    print ('Quitting...') #Informa ao usuário que um processo de SAÍDA está iniciando
-                    pygame.quit()  # Close Window (Fecha a janela)
-                    quit()  # end pygame (Fecha de fato o pygame)
+            # for event in pygame.event.get():  # TODOS os EVENTOS VIRÃO
+            #    if event.type == pygame.QUIT:  # Quando o evento for do tipo quit
+            #        pygame.quit()  # Close Window (Fecha a janela)
+            #        quit()  # end pygame (Fecha de fato o pygame)
--- code/menu.py
+++ code/menu.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window): #método construtor ativado
+        self.window = window #chamando a janela do manu
 
     def run(self, ):
         pass
